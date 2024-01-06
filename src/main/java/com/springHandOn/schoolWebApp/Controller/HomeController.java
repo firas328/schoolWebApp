93bb1fbb0b1c9e4a0df724e4fe95259ea65a1419
firas328 <firraslassoied8@gmail.com>
2024-01-06 20:20:18 +0100

    add front template and assests

--- src/main/java/com/springHandOn/schoolWebApp/Controller/HomeController.java
+++ src/main/java/com/springHandOn/schoolWebApp/Controller/HomeController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
     @RequestMapping(value={"", "/", "home"})
-    public String displayHomePage(Model model) {
-        model.addAttribute("username", "Firas");
+    public String displayHomePage() {
         return "home.html";
     }
 }
