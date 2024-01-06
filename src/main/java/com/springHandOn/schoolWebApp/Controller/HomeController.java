ecb1c11c9f64486518dcd032823d3b3e14e8b226
firas328 <firraslassoied8@gmail.com>
2024-01-06 13:35:23 +0100

    init

---
+++ src/main/java/com/springHandOn/schoolWebApp/Controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.springHandOn.schoolWebApp.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value={"", "/", "home"})
+    public String displayHomePage(Model model) {
+        model.addAttribute("username", "John Doe");
+        return "home.html";
+    }
+}
