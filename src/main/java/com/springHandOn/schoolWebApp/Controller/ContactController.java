c88b4f8d6c3d744bf142c16b68d1a367fa93eb06
firas328 <firraslassoied8@gmail.com>
2024-01-07 20:11:12 +0100

    add contact message form handller in contact controller

--- src/main/java/com/springHandOn/schoolWebApp/Controller/ContactController.java
+++ src/main/java/com/springHandOn/schoolWebApp/Controller/ContactController.java
@@ -1,12 +1,28 @@
 package com.springHandOn.schoolWebApp.Controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ContactController {
+    Logger logger = LoggerFactory.getLogger(ContactController.class.getName());
     @RequestMapping(path="/contact")
     public String displayContactPage(){
         return "contact.html";
+    }
+    @PostMapping(path="saveMsg")
+    public ModelAndView saveMessage(@RequestParam String name, @RequestParam String mobileNum,@RequestParam String email,
+                                    @RequestParam String subject,@RequestParam String message ){
+        logger.info("Name :"+name);
+        logger.info("mobile number :"+mobileNum);
+        logger.info("email :"+email);
+        logger.info("subject :"+subject);
+        logger.info("message :"+message);
+        return new ModelAndView("redirect:/contact");
     }
 }
