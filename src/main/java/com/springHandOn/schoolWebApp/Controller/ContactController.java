889536a6a218776dd5271e05544b888b73e86aac
firas328 <firaslassoied8@gmail.com>
2024-01-08 00:43:53 +0100

    use @Slf4j instead of instantiate logger object

--- src/main/java/com/springHandOn/schoolWebApp/Controller/ContactController.java
+++ src/main/java/com/springHandOn/schoolWebApp/Controller/ContactController.java
@@ -2,6 +2,7 @@ package com.springHandOn.schoolWebApp.Controller;
 
 import com.springHandOn.schoolWebApp.model.Contact;
 import com.springHandOn.schoolWebApp.services.ContactService;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
+@Slf4j
 @Controller
 public class ContactController {
     Logger logger = LoggerFactory.getLogger(ContactController.class.getName());
@@ -26,6 +27,7 @@ public class ContactController {
     }
     @PostMapping(path="saveMsg")
     public ModelAndView saveMessage(Contact contact){
+        log.info("saving");
         contactService.saveMessageDetails(contact);
         return new ModelAndView("redirect:/contact");
     }
