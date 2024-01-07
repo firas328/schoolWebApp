0eb2144ce148ec4eea3891adbb507504a7f436bb
firas328 <firraslassoied8@gmail.com>
2024-01-08 00:08:21 +0100

    solve lombok dependency issue

--- src/main/java/com/springHandOn/schoolWebApp/model/Holiday.java
+++ src/main/java/com/springHandOn/schoolWebApp/model/Holiday.java
@@ -4,12 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class Holiday {
+
     private final String day;
     private final String reason;
     private final Type type;
-    public enum Type{
-        FESTIVAL,FEDERAL
+
+    public enum Type {
+        FESTIVAL, FEDERAL
     }
 }
