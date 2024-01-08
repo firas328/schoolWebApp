ccc96ea09118e0cf1e57b6af61fa47475e1989ba
firas328 <firaslassoied8@gmail.com>
2024-01-08 18:58:20 +0100

    dispaly holidays based on path variabale

--- src/main/java/com/springHandOn/schoolWebApp/Controller/HolidaysController.java
+++ src/main/java/com/springHandOn/schoolWebApp/Controller/HolidaysController.java
@@ -3,6 +3,7 @@ import com.springHandOn.schoolWebApp.model.Holiday;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Arrays;
@@ -18,6 +19,33 @@ public class HolidaysController {
                                   @RequestParam(required = false) boolean federal) {
         model.addAttribute("festival",festival);
         model.addAttribute("federal",federal);
+        List<Holiday> holidays = Arrays.asList(
+                new Holiday(" Jan 1 ","New Year's Day", Holiday.Type.FESTIVAL),
+                new Holiday(" Oct 31 ","Halloween", Holiday.Type.FESTIVAL),
+                new Holiday(" Nov 24 ","Thanksgiving Day", Holiday.Type.FESTIVAL),
+                new Holiday(" Dec 25 ","Christmas", Holiday.Type.FESTIVAL),
+                new Holiday(" Jan 17 ","Martin Luther King  Day", Holiday.Type.FEDERAL),
+                new Holiday(" July 4 ","Independence Day", Holiday.Type.FEDERAL),
+                new Holiday(" Sep 5 ","Labor Day", Holiday.Type.FEDERAL),
+                new Holiday(" Nov 11 ","Veterans Day", Holiday.Type.FEDERAL)
+        );
+        Holiday.Type[] types = Holiday.Type.values();
+        for (Holiday.Type type : types) {
+            model.addAttribute(type.toString(),
+                    (holidays.stream().filter(holiday -> holiday.getType().equals(type)).collect(Collectors.toList())));
+        }
+        return "holidays.html";
+    }
+    @GetMapping("/holidays/{path}")
+    public String displayHolidays(Model model, @PathVariable String path) {
+        switch (path) {
+            case "all" -> {
+                model.addAttribute("festival", true);
+                model.addAttribute("federal", true);
+            }
+            case "federal" -> model.addAttribute("federal", true);
+            case "festival" -> model.addAttribute("festival", true);
+        }
         List<Holiday> holidays = Arrays.asList(
                 new Holiday(" Jan 1 ","New Year's Day", Holiday.Type.FESTIVAL),
                 new Holiday(" Oct 31 ","Halloween", Holiday.Type.FESTIVAL),
