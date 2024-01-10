a8310b9ea24e3cf335418f644f7ee7534a00bf2c
firas328 <firaslassoied8@gmail.com>
2024-01-10 18:27:26 +0100

    use in memory authentication

--- src/main/java/com/springHandOn/schoolWebApp/config/ApplicationSecurityConfig.java
+++ src/main/java/com/springHandOn/schoolWebApp/config/ApplicationSecurityConfig.java
@@ -2,27 +2,46 @@ package com.springHandOn.schoolWebApp.config;
 
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+@EnableWebSecurity
 public class ApplicationSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.csrf((csrf) -> csrf.disable())
-                .authorizeHttpRequests((requests) -> requests.requestMatchers("", "/", "/home").permitAll()
+                .authorizeHttpRequests((requests) -> requests.requestMatchers( "/", "/home").permitAll()
                         .requestMatchers("/holidays/**").permitAll()
                         .requestMatchers("/contact").permitAll()
                         .requestMatchers("/saveMsg").permitAll()
                         .requestMatchers("/courses").permitAll()
-                        .requestMatchers("/about").permitAll()
-                        .requestMatchers("/assets/**").permitAll()
-                        .requestMatchers("/dashboard").authenticated())
+                        .requestMatchers("/dashboard").authenticated()
+                )
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
-        return http.build();
-    }
+        return http.build();}
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
 
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("123")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("321")
+                .roles("USER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
 }
 
 
